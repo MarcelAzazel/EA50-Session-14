71859ad6052c696c7f722e767af94fdd37ab0e83
Marcel <119822011+MarcelAzazel@users.noreply.github.com>
2024-10-28 16:20:02 -0700

    Update Data visualization 2.py

--- Data visualization 2.py
+++ Data visualization 2.py
@@ -1,12 +1,13 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
 df = pd.read_csv('C:\Arquivos\Documentos\Courses\EA50\Coding\data.csv')
 
-plt.plot(df['TIME (year.decimal fraction)'], df['Greenland mass (Gt)'])
-plt.plot(df['TIME (year.decimal fraction)'], df['Antarctica mass (Gt)'])
+plt.plot(df['TIME (year.decimal fraction)'], df['Greenland mass (Gt)'], label='Greenland mass')
+plt.plot(df['TIME (year.decimal fraction)'], df['Antarctica mass (Gt)'], label='Antarctica mass')
 plt.xlabel('Year')
 plt.ylabel('Anomalies')
 plt.title('Land Ice Mass')
+plt.legend()
 plt.show()
